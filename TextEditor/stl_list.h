c8af31e80ecd128960b5e33caa9d16f7a396b9f9
hanchaoqi <hanchao_qi@qq.com>
2016-01-27 19:55:52 +0800

    stl_list.h

--- TextEditor/stl_list.h
+++ TextEditor/stl_list.h
@@ -1,148 +1,176 @@
-#include <iostream>
-#include <string>
-using namespace std;
-typedef unsigned long size_type;
-
-template<typename InputIterator> void distance(InputIterator first, InputIterator last, size_type& result);
-
-template<class Elem> struct Link
-{
-	Link<Elem>* prev;
-	Link<Elem>* succ;
-	Elem val;
+template<class T> struct __list_node{
+	typedef void* void_pointer;
+	void_pointer next;
+	void_pointer prev;
+	T data;
 };
-template<class Elem> class list{
-	Link<Elem>* first;
-	Link<Elem>* last;
-public:
-	
-	typedef Elem value_type;
-	typedef Link<Elem>* link_type;
-	class iterator;
-	class const_iterator;
+
+template<class T, class Ref, class Ptr> struct __list_iterator{
+	typedef __list_iterator<T, T&, T*> iterator;
+	typedef __list_iterator<T, const T&, const T*> const_iterator;
+	typedef __list_iterator<T, Ref, Ptr> self;
+
+	typedef bidirectional_iterator_tag iterator_category;
+	typedef T value_type;
+	typedef Ref reference;
+	typedef Ptr pointer;
+	typedef __list_node<T>* link_type;
+	typedef size_t size_type;
+	typedef ptrdiff_t difference_type;
 
 	link_type node;
 
-	iterator begin();
-	const_iterator begin() const;
-	iterator end();
-	const_iterator end() const;
+	__list_iterator(link_type x) :node(x){}
+	__list_iterator(){}
+	__list_iterator(const iterator& x) :node(x.node){}
 
-	size_type size();
+	bool operator==(const self& x)const{ return node == x.node; }
+	bool operator!=(const self& x)const{ return node != x.node; }
 
-	iterator insert(iterator p, const Elem& v);
-	iterator erase(iterator p);
+	reference operator*()const{ return (*node).data; }
 
-	void push_back(const Elem& v);
-	void push_front(const Elem& v);
-	void pop_front();
-	void pop_back();
+	pointer operator->()const{ return &(operator*()); }
 
-	Elem& front();
-	Elem& back();
-};
-
-template<class Elem> class list<Elem>::iterator{
-	Link<Elem>* curr;
-public:
-	iterator(Link<Elem>* p) :curr(p){}
-
-	iterator& operator++(){ curr = curr->succ; return *this; }
-	iterator& operator--(){ curr = curr->prev; return *this; }
-	Elem& operator*(){ return curr->val; }
-	iterator operator->(){ return &(operator*()); }
-	
-	bool operator==(const iterator& b)const{ return curr == b.curr; }
-	bool operator!=(const iterator& b)const{ return curr != b.curr; }
-};
-template<class Elem> class list<Elem>::const_iterator{
-	const Link<Elem>* curr;
-public:
-	const_iterator(const Link* p) :curr(p){}
-
-	const_iterator& operator++(){ curr = curr->succ; return const *this; }
-	const_iterator& operator--(){ curr = curr->prev; return const *this; }
-	const Elem& operator*(){ return curr->val; }
-
-	bool operator==(const const_iterator& b)const{ return curr == b.curr; }
-	bool operator!=(const const_iterator& b)const{ return curr != b.curr; }
-};
-
-template<class Elem> typename list<Elem>::iterator list<Elem>::begin()
-{
-	return first;
-}
-template<class Elem> typename list<Elem>::const_iterator list<Elem>::begin() const
-{
-	return first;
-}
-template<class Elem> typename list<Elem>::iterator list<Elem>::end()
-{
-	return last;
-}
-template<class Elem> typename list<Elem>::const_iterator list<Elem>::end() const
-{
-	return last;
-}
-template<class Elem> typename size_type list<Elem>::size()
-{
-	size_type result = 0;
-	distance(begin(), end(), result);
-	return result;
-}
-
-template<class Elem> typename list<Elem>::iterator list<Elem>::insert(iterator p, const Elem& v)
-{
-	link_type tmp = &(Link(v));
-	p->succ->prev = tmp;
-	tmp->succ = p->succ;
-	p->succ = tmp;
-	tmp->prev = p;
-	return p;
-}
-template<class Elem> typename list<Elem>::iterator list<Elem>::erase(iterator p)
-{
-	if (p == this->begin())
-	{
-		this->first = p->succ;
-		delete p;
-		return this;
+	self& operator++(){
+		node = (link_type)((*node).next);
+		return *this;
 	}
-	p->prev->succ = p->succ;
-	p->succ->prev = p->prev;
-	delete p;
-	return this;
+	self& operator++(int){
+		self tmp = *this;
+		++*this;
+		return tmp;
+	}
+	self& operator--(){
+		node = (link_type)((*node).prev);
+		return *this;
+	}
+	self& operator--(int){
+		self tmp = *this;
+		--*this;
+		return tmp;
+	}
+};
+#ifndef __STL_CLASS_PARTIAL_SPECIALIZATION
+template<class T, class Ref, class Ptr> inline bidirectional_iterator_tag 
+iterator_category(const __list_iterator<T, Ref, Ptr>&)
+{
+	return bidirectional_iterator_tag();
 }
 
-template<class Elem> void list<Elem>::push_back(const Elem& v)
-{
-	link_type tmp = &(Link(v));
-	this->last->prev->succ = tmp;
-	tmp->prev = this->last->prev;
-	tmp->succ = this->last;
-	this->last->prev = tmp;
-}
-template<class Elem> void list<Elem>::push_front(const Elem& v)
-{
-	link_type tmp = &(Link(v));
-	this->first->prev = tmp;
-	tmp->succ = this->first;
-	this->first = tmp;
-}
-template<class Elem> void list<Elem>::pop_front()
-{
-	erase(this->first);
-}
-template<class Elem> void list<Elem>::pop_back()
-{
-	erase(this->last->prev);
+template<class T, class Ref, class Ptr> inline T* 
+value_type(const __list_iterator<T, Ref, Ptr>&){
+	return 0;
 }
 
-template<class Elem> Elem& list<Elem>::front()
-{
-	return this->begin()->val;
+template<class T, class Ref, class Ptr> inline ptrdiff_t*
+distance_type(const __list_iterator<T, Ref, Ptr>&){
+	return 0;
 }
-template<class Elem> Elem& list<Elem>::back()
-{
-	return this->end()->prev->val;
-}
+#endif /*__STL_CLASS_PARTIAL_SPECIALIZATION*/
+
+template<class T, class Alloc = alloc> class list{
+protected:
+	typedef void* void_pointer;
+	typedef  __list_node<T> list_node;
+	typedef simple_alloc<list_node, Alloc> list_node_allocator;
+public:
+	typedef T value_type;
+	typedef value_type* pointer;
+	typedef const value_type* const_pointer;
+	typedef value_type& reference;
+	typedef const value_type& const_reference;
+	typedef list_node* link_type;
+	typedef size_t size_type;
+	typedef ptrdiff_t difference_type;
+
+public:
+	typedef __list_iterator<T, T&, T*>	iterator;
+	typedef __list_iterator<T, const T&, const T*> const_iterator;
+
+#ifdef __STL_CLASS_PARTIAL_SPECIALIZATION
+	typedef reverse_iterator<const_iterator> const_reverse_iterator;
+	typedef reverse_iterator<iterator> reverse_iterator;
+#else
+	typedef reverse_bidirectional_iterator<const_iterator, value_type, 
+		const_reference, difference_type> const_reverse_iterator;
+	typedef reverse_bidirectional_iterator<iterator, value_type, 
+		reference, difference_type> reverse_iterator;
+#endif /*__STL_CLASS_PARTIAL_SPECIALIZATION*/
+protected:
+	link_type node;
+	link_type get_node(){ return list_node_allocator::allocate(); }
+	void put_node(link_type p){ list_node_allocator::deallocate(p); }
+
+	link_type create_node(const T& x){
+		link_type p = get_node();
+		__STL_TRY{
+			construct(&p->data, x);
+		}
+		__STL_UNWIND(put_node(p));
+		return p;
+	}
+	void destroy_node(link_type p){
+		destroy(&p->data);
+		put_node(p);
+	}
+
+	void empty_initialize(){
+		node = get_node();
+		node->prev = node;
+		node->next = node;
+	}
+	void fill_initialize(size_type n, const T& value)
+	{
+		empty_initialize();
+		__STL_TRY{
+			insert(begin(), n, value);
+		}
+		__STL_UNWIND(clear(); put_node(node));
+	}
+#ifdef __STL_MEMBER_TEMPLATES  
+	template<class InputIterator>
+	void range_initialize(InputIterator first, InputIterator last)
+	{
+		empty_initialize();
+		__STL_TRY{
+			insert(begin(), first, last);
+		}
+		__STL_UNWIND(clear(); put_node(node));
+	}
+#else
+	void range_initialzie(const T* first, const T* last)
+	{
+		empty_initialize();
+		__STL_TRY{
+			insert(begin(), first, last);
+		}
+		__STL_UNWIND(clear(); put_node(node));
+	}
+	void range_initialize(const_iterator first, const_iterator last)
+	{
+		empty_initialize();
+		__STL_TRY{
+			insert(begin(), first, last);
+		}
+		__STL_UNWIND(clear(); put_node(node));
+	}
+#endif
+
+public:
+	list(){ empty_initialize(); }
+	iterator begin(){ return (link_type)((*node).next); }
+	const_iterator begin()const { return (link_type)((*node).next); }
+
+	iterator end(){ return node; }
+	const_iterator end()const{ return node; }
+
+	reverse_iterator rbegin(){ return reverse_iterator(begin()); }
+	const_reverse_iterator rbegin()const{ return reverse_iterator(begin()); }
+
+	bool empty()const{ return node->next == node; }
+	size_type size() const{
+		size_type result = 0;
+		distance(begin(), end(), result);
+		return result;
+	}
+};
